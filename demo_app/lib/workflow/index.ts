b9486def7a9f686668547bf28704391945eeddc4
Simon Mulquin <simon@octree.ch>
2022-11-15 00:09:14 +0000

    style: 🏷️ Add typing to lib

---
+++ demo_app/lib/workflow/index.ts
@@ -0,0 +1,116 @@
+import { TNode, Methods } from "./types";
+
+//POC
+export const demoNodes: Array<TNode & Methods> = [];
+
+const newNode = (def: Partial<TNode> & Pick<TNode, "id" | "pres" | "pros">) => {
+  //state
+  let node = {
+    gate: "AND",
+    flows: [],
+    status: "initiated",
+    ...def,
+  } as TNode & Methods;
+
+  //actions
+  node.undo = () => console.log("can't undo an undone node");
+
+  node.do = () => {
+    node.status = "done";
+    console.log(node.id, node.status);
+    demoNodes[node.id].undo = () => {
+      node.status = "undone";
+      demoNodes[node.id].undo = node.undo;
+      return node.emit("close");
+    };
+
+    return node.emit("open");
+  };
+
+  //flow handler
+  node.flow = (op, emiterId) => {
+    switch (op) {
+      case "open":
+        if (node.flows?.includes(emiterId)) {
+          console.log(node.id, " is up to date");
+          return;
+        } else if (node.pres?.includes(emiterId)) {
+          node.flows = [...node.flows, emiterId];
+        } else {
+          console.log(emiterId, " is not a predecessor of ", node.id);
+        }
+        break;
+
+      case "close":
+        node.flows = node.flows?.filter(id => id != emiterId);
+        console.log(emiterId, " closed flow to ", node.id);
+        break;
+
+      default:
+        console.log(op, " is not a flow operation");
+        return;
+    }
+
+    switch (node.gate) {
+      case "AND":
+        if (node.flows?.length === node.pres?.length) {
+          return node.do();
+        }
+        console.log(node.id, " doesn't match AND condition");
+        return demoNodes[node.id].undo();
+
+      case "OR":
+        if (node.flows.length >= 1) {
+          return node.do();
+        }
+        console.log(node.id, " doesn't match OR condition");
+        return demoNodes[node.id].undo();
+
+      case "XOR":
+        if (node.flows.length === 1) {
+          return node.do();
+        }
+        console.log(node.id, " doesn't match XOR condition");
+        return demoNodes[node.id].undo();
+
+      default:
+        console.log(node.gate, " is not a covered condition");
+        return;
+    }
+  };
+
+  //emiter
+  node.emit = op =>
+    node.pros
+      ? node.pros.forEach(id => demoNodes[id].flow(op, node.id))
+      : console.log("END");
+
+  return demoNodes.push(node);
+};
+
+//default gate will be AND
+newNode({ pres: null, id: 0, pros: [1] });
+newNode({ pres: [0], id: 1, pros: [2] });
+newNode({ pres: [1], id: 2, pros: [3, 4] });
+newNode({ pres: [2], id: 3, pros: [5] });
+newNode({ pres: [2], id: 4, pros: [5] });
+newNode({ pres: [3, 4], id: 5, pros: [7], gate: "AND" });
+newNode({ pres: null, id: 6, pros: [7] });
+newNode({ pres: [5, 6], id: 7, pros: [9], gate: "OR" });
+newNode({ pres: null, id: 8, pros: [9] });
+newNode({ pres: [7, 8], id: 9, pros: null, gate: "XOR" });
+
+console.log("START 8");
+demoNodes[8].do();
+console.log("START 0");
+demoNodes[0].do();
+console.log("START 6");
+demoNodes[6].do();
+console.log("CLOSE 8");
+demoNodes[8].emit("close");
+console.log("CLOSE 5");
+demoNodes[5].emit("close");
+console.log("CLOSE 6");
+demoNodes[6].emit("close");
+console.log("CLOSE 0");
+demoNodes[0].emit("close");
---
+++ demo_app/lib/workflow/types.ts
@@ -0,0 +1,17 @@
+export type Operation = "open" | "close";
+
+export type Methods = {
+  do: () => void;
+  undo: () => void;
+  flow: (op: Operation, emiterId: number) => void | undefined;
+  emit: (op: Operation) => void;
+};
+
+export type TNode = {
+  id: number;
+  gate: "AND" | "OR" | "XOR";
+  status: "done" | "undone" | "initiated";
+  flows: Array<number>;
+  pres: Array<number> | null;
+  pros: Array<number> | null;
+};
